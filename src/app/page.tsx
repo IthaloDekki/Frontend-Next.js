1711f6d22585c3906d15a46cd363e2ac13323070
IthaloDekki <ithalomedei76@gmail.com>
2024-03-06 00:17:33 -0300

    terminando o projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,146 @@
-import Image from "next/image";
+"use client"
+
+import React, { useEffect, useState } from "react";
+
+import { Task } from "./task"; //Interface de tasks as is in the backend with NestJS
+
+const getDone = (tasks: Task[]) => {
+    return tasks.filter(task => task.checked)
+}
+
+const getPending = (tasks: Task[]) => {
+    return tasks.filter(task => !task.checked)
+}
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [tasks, setTasks] = useState<Task[]>([])
+    const [idCounter, setIdCounter] = useState(0)
+    const [progress, setProgress] = useState(0)
+    const [filter , setFilter] = useState('all')
+ 
+    const addTask = (taskText: string) => {
+     const newTask = new Task(idCounter, taskText, false, 0)
+     setTasks((prevTasks) => [...prevTasks, newTask])
+     setIdCounter(idCounter + 1)
+    }
+ 
+    const changeProcess = () => {
+     const completedTasks = tasks.filter(task => task.checked).length
+     const totalTask = tasks.length
+     const percentage = totalTask > 0 ? (completedTasks / totalTask) * 100 : 0
+     setProgress(percentage)
+    }
+    const statusChange = (taskId: number) => {
+      setTasks((tasks) => 
+        tasks.map((task) =>
+          task.id === taskId ? { ...task, checked: !task.checked } : task
+        )
+      );
+    };
+    const filterTasks = () => {
+      switch (filter) {
+        case "pending":
+          return getPending(tasks);
+        case "done":
+          return getDone(tasks);
+        default:
+          return tasks;
+      }
+    }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const deleteDone = () => {
+      setTasks(getPending(tasks))
+    };
+    
+    useEffect(() => {
+      changeProcess()
+    }, [tasks]);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+   return (
+    <div className=" h-screen flex items-center justify-center ">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+         <h1 className="text-red-400 text-4xl">TO-DO LIST</h1>
+         <div className="p-5 border-2 border-red-400 rounded">
+            <AddTask onAddTask={addTask}/>
+            <ProgressBar percentage={progress}/>
+            <ListTasks OnStatusChange={statusChange} tasks={filterTasks()}/>
+            <hr className="h-px my-8 bg-red-200 border-0 dark:bg-red-400"></hr>
+            <OptionsLine tasks={tasks} filter={filter} OnFilterChange={setFilter} OnDeleteDone={deleteDone}/>
+         </div>
+      </form>
+    </div>
+   );
 }
+
+
+function AddTask({ onAddTask }: {onAddTask: (taskText: string) => void}){
+  const [taskText, setTaskText] = useState('')
+
+  const handleAdd = () =>{
+    if (taskText.trim() !== ''){
+      onAddTask(taskText)
+      setTaskText('')
+    }
+  }
+
+  return (
+    <div className="w-12/12 flex space-x-4 text-xl">
+      <input onChange={(e) => setTaskText(e.target.value)} value={taskText} className="shadow appearance-none border rounded w-64 py-2 px-3 text-red-500 leading-tight focus:outline-none focus:shadow-outline" id="task" type="text" placeholder="Sou uma tarefa:)"/>
+      <button onClick={handleAdd} className="bg-red-400  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2">Enter</button>
+    </div>
+    
+  )
+}
+
+function ProgressBar({percentage}: {percentage: number}){
+    return(
+      <>
+        <div className="w-full mt-2 bg-red-500 rounded-full h-1.5 mb-4 dark:bg-red-400">
+          <div style= {{width: `${percentage}%`}} className={`bg-red-400 h-1.5 rounded-full dark:bg-red-500`}></div>
+        </div>
+      </>
+    )
+}
+
+function AddedTask({task, OnStatusChange}: {OnStatusChange: (taskId: number) => void; task: Task}){
+  return(
+    <div className="w-12/12 flex items-center space-x-2">
+      <input onChange={(e) => OnStatusChange(task.id)} 
+      type="checkbox" checked={task.checked} className="w-6 h-6 text-white-600 bg-gray-100 border-red-300 rounded focus:ring-red-500 dark:focus:ring-red-800 dark:ring-offset-red-800 focus:ring-2 dark:bg-red-400 dark:border-red-400"/>
+      <label className="text-xl font-medium text-white">{task.text}</label>
+    </div>
+  )
+}
+
+
+function ListTasks({ tasks, OnStatusChange}: {OnStatusChange: (taskId:number) => void; tasks: Task[]}){
+  const listTasks = tasks.map(task =>
+    <AddedTask key={task.id} OnStatusChange={OnStatusChange} task={task}/>
+  )
+  return (
+    <>
+      {listTasks}
+    </>
+  )
+}
+
+function OptionsLine({ tasks, filter, OnFilterChange, OnDeleteDone }: { tasks: Task[]; filter: string; OnFilterChange: (filter: string) => void; OnDeleteDone: () => void}) {
+  
+
+  return (
+    <div className="w-12/12 flex items-center justify-center space-x-12">
+      <p>Tasks pending: {getPending(tasks).length}</p>
+      <button onClick={() => OnFilterChange("all")} className={`${
+        filter === "all" ? 'border-b-2 border-red-400 text-red-200' : ''
+      }`}>All</button>
+      <button onClick={() => OnFilterChange("pending")} className={`${
+        filter === "pending" ? 'border-b-2 border-red-400 text-red-200' : ''
+      }`}>Pending</button>
+      <button onClick={() => OnFilterChange("done")} className={`${
+        filter === "done" ? 'border-b-2 border-red-400 text-red-200' : ''
+      }`}>Done</button>
+      <button onClick={() => OnDeleteDone()} className="px-4 cursor-pointer border-2 border-red-400 rounded text-red-200">Clean Completed</button>
+  </div>
+  );
+};
